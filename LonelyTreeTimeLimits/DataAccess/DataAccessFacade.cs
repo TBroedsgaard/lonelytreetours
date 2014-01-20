e3545b93c90c0e52572d859fee0c926a489bca3a
TBroedsgaard <troels@broedsgaard.com>
2014-01-20 12:59:31 +0100

    start work on ModelFacade

---
+++ LonelyTreeTimeLimits/DataAccess/DataAccessFacade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+
+namespace DataAccess
+{
+    public class DataAccessFacade
+    {
+        public List<ICustomer> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ISale> GetAllSales()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LonelyTreeTimeLimits/Model/ModelFacade.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+using DataAccess;
+
+namespace Model
+{
+    public class ModelFacade
+    {
+        private List<Customer> customers;
+        private List<Sale> sales;
+        private DataAccessFacade dataAccessFacade;
+
+        public ModelFacade()
+        {
+            dataAccessFacade = new DataAccessFacade();
+        }
+
+        public List<ICustomer> GetAllCustomers()
+        {
+            List<ICustomer> iCustomers;
+
+            if (customers == null)
+            {
+                iCustomers = dataAccessFacade.GetAllCustomers();
+                foreach (ICustomer iCustomer in iCustomers)
+                {
+                    Customer customer = new Customer();
+                    customer.BirthDate = iCustomer.BirthDate;
+                    customer.Comment = iCustomer.Comment;
+                    customer.Country = iCustomer.Country;
+                    customer.CreatedDate = iCustomer.CreatedDate;
+                    customer.CustomerType = iCustomer.CustomerType;
+                    customer.Email = iCustomer.Email;
+                }
+            }
+
+            return null;
+        }
+
+        public List<ISale> GetAllSales()
+        {
+            if (sales == null)
+            {
+            }
+
+            List<ISale> iSales = sales.Cast<ISale>().ToList();
+
+            return iSales;
+        }
+
+
+    }
+}
