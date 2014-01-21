4f9fd928d7ecda58ae0d28e79269cf8d28c26d1c
TBroedsgaard <troels@broedsgaard.com>
2014-01-21 07:20:37 +0100

    add Model and Data facades
    Implementations not finished

--- LonelyTreeTimeLimits/DataAccess/DataAccessFacade.cs
+++ LonelyTreeTimeLimits/DataAccess/DataAccessFacade.cs
@@ -9,7 +9,37 @@ namespace DataAccess
 {
     public class DataAccessFacade
     {
+        public bool CreateCustomer(ICustomer iCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCustomer(ICustomer iCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteCustomer(ICustomer iCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<ICustomer> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateSale(ISale iSale)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateSale(ISale iSale)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteSale(ISale iSale)
         {
             throw new NotImplementedException();
         }
--- LonelyTreeTimeLimits/Model/ModelFacade.cs
+++ LonelyTreeTimeLimits/Model/ModelFacade.cs
@@ -19,39 +19,123 @@ namespace Model
             dataAccessFacade = new DataAccessFacade();
         }
 
-        public List<ICustomer> GetAllCustomers()
+        public bool CreateCustomer(ICustomer iCustomer)
         {
-            List<ICustomer> iCustomers;
-
-            if (customers == null)
+            if (dataAccessFacade.CreateCustomer(iCustomer))
             {
-                iCustomers = dataAccessFacade.GetAllCustomers();
-                foreach (ICustomer iCustomer in iCustomers)
-                {
-                    Customer customer = new Customer();
-                    customer.BirthDate = iCustomer.BirthDate;
-                    customer.Comment = iCustomer.Comment;
-                    customer.Country = iCustomer.Country;
-                    customer.CreatedDate = iCustomer.CreatedDate;
-                    customer.CustomerType = iCustomer.CustomerType;
-                    customer.Email = iCustomer.Email;
-                }
+                Customer customer = new Customer(iCustomer);
+                customers.Add(customer);
+
+                return true;
             }
 
-            return null;
+            return false;
         }
 
-        public List<ISale> GetAllSales()
+        public bool UpdateCustomer(ICustomer iCustomer)
+        {
+            if (dataAccessFacade.UpdateCustomer(iCustomer))
+            {
+                Customer customer = new Customer(iCustomer); // Creating a new object since I need to add it to the list anyway
+                // find customer with id, delete from list, add the new one
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool DeleteCustomer(ICustomer iCustomer)
+        {
+            if (dataAccessFacade.DeleteCustomer(iCustomer))
+            { 
+                Customer customer = new Customer(iCustomer); // no need to create new object, just find the one from the iCustomer id
+                // find customer with id, set Deleted bit
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public List<ICustomer> GetCustomers()
+        {
+            if (customers == null)
+            {
+                loadCustomers();
+            }
+
+            List<ICustomer> iCustomers = customers.Cast<ICustomer>().ToList();
+            return iCustomers;
+        }
+
+        private void loadCustomers()
+        {
+            List<ICustomer> iCustomers = dataAccessFacade.GetAllCustomers();
+
+            foreach (ICustomer iCustomer in iCustomers)
+            {
+                Customer customer = new Customer(iCustomer);
+                customers.Add(customer);
+            }
+        }
+
+        public bool CreateSale(ISale iSale)
+        {
+            Sale sale = new Sale(iSale);
+            if (dataAccessFacade.CreateSale(iSale))
+            {
+                sales.Add(sale);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool UpdateSale(ISale iSale)
+        {
+            if (dataAccessFacade.UpdateSale(iSale))
+            {
+                Sale sale = new Sale(iSale);
+                // find sale with id, delete from list, add the new one
+            }
+            return false;
+        }
+
+        public bool DeleteSale(ISale iSale)
+        {
+            if (dataAccessFacade.DeleteSale(iSale))
+            {
+                Sale sale = new Sale(iSale);
+                sales.Remove(sale); // TODO: BUG!!! Needs identifier, just as Update does - try typecasting and catch any exceptions? No more ID...
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public List<ISale> GetSales()
         {
             if (sales == null)
             {
+                loadSales();
             }
 
             List<ISale> iSales = sales.Cast<ISale>().ToList();
-
             return iSales;
         }
 
+        private void loadSales()
+        {
+            List<ISale> iSales = dataAccessFacade.GetAllSales();
 
+            foreach (ISale iSale in iSales)
+            {
+                Sale sale = new Sale(iSale);
+                sales.Add(sale);
+            }
+        }
     }
 }
