82ea8e65c8ff36e9918ee163428accdade3c4dc4
Biacki <cille@valeria.dk>
2014-01-28 22:45:13 +0100

    BookingController and PaymentRuleCatalogController
    Tror de er fine nok

---
+++ LonelyTreeTimeLimits/DataAccess/Controllers/BookingController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Entities;
+using Interfaces;
+
+namespace DataAccess.Controllers
+{
+    internal class BookingController : DataController<BookingEntity>
+    {
+        private const string FILENAME = "booking.bin";
+
+        public BookingController()
+        {
+            binaryHelper = new BinaryHelper<BookingEntity>();
+            entities = binaryHelper.Load(FILENAME);
+            setNextId();
+        }
+
+        internal IBooking Create(IBooking ib)
+        {
+            ib.Id = getNextId();
+            ib.LastUpdated = DateTime.Now;
+            ib.Deleted = false;
+            BookingEntity be = new BookingEntity(ib);
+            entities.Add(be);
+
+            try
+            {
+                binaryHelper.Save(FILENAME, entities);
+            }
+            catch
+            {
+                ib.Deleted = true;
+                throw;
+            }
+
+
+            return ib;
+        }
+
+        internal List<IBooking> GetAll()
+        {
+            return entities.Cast<IBooking>().ToList();
+        }
+
+        internal IBooking Update(IBooking ib)
+        {
+            BookingEntity oldBe = find(ib);
+            ib.LastUpdated = DateTime.Now;
+            BookingEntity newBe = new BookingEntity(ib);
+
+            entities.Remove(oldBe);
+            entities.Add(newBe);
+
+            binaryHelper.Save(FILENAME, entities);
+
+            return (IBooking)newBe;
+        }
+
+        internal IBooking Delete(IBooking ib)
+        {
+            ib.Deleted = true;
+            return Update(ib);
+        }
+
+        private BookingEntity find(IBooking ib)
+        {
+            foreach (BookingEntity be in entities)
+            {
+                if (be.Id == ib.Id)
+                {
+                    return be;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ LonelyTreeTimeLimits/DataAccess/Controllers/PaymentRuleCatalogController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Entities;
+using Interfaces;
+
+namespace DataAccess.Controllers
+{
+    internal class PaymentRuleCatalogController : DataController<PaymentRuleCatalogEntity>
+    {
+        private const string FILENAME = "paymentrulecatalog.bin";
+
+        public PaymentRuleCatalogController()
+        {
+            binaryHelper = new BinaryHelper<PaymentRuleCatalogEntity>();
+            entities = binaryHelper.Load(FILENAME);
+            setNextId();
+        }
+
+        internal IPaymentRuleCatalog Create(IPaymentRuleCatalog iprc)
+        {
+            iprc.Id = getNextId();
+            iprc.LastUpdated = DateTime.Now;
+            iprc.Deleted = false;
+            PaymentRuleCatalogEntity prce = new PaymentRuleCatalogEntity(iprc);
+            entities.Add(prce);
+
+            try
+            {
+                binaryHelper.Save(FILENAME, entities);
+            }
+            catch
+            {
+                iprc.Deleted = true;
+                throw;
+            }
+
+
+            return iprc;
+        }
+
+        internal List<IPaymentRuleCatalog> GetAll()
+        {
+            return entities.Cast<IPaymentRuleCatalog>().ToList();
+        }
+
+        internal IPaymentRuleCatalog Update(IPaymentRuleCatalog iprc)
+        {
+            PaymentRuleCatalogEntity oldPrce = find(iprc);
+            iprc.LastUpdated = DateTime.Now;
+            PaymentRuleCatalogEntity newPrce = new PaymentRuleCatalogEntity(iprc);
+
+            entities.Remove(oldPrce);
+            entities.Add(newPrce);
+
+            binaryHelper.Save(FILENAME, entities);
+
+            return (IPaymentRuleCatalog)newPrce;
+        }
+
+        internal IPaymentRuleCatalog Delete(IPaymentRuleCatalog iprc)
+        {
+            iprc.Deleted = true;
+            return Update(iprc);
+        }
+
+        private PaymentRuleCatalogEntity find(IPaymentRuleCatalog iprc)
+        {
+            foreach (PaymentRuleCatalogEntity prce in entities)
+            {
+                if (prce.Id == iprc.Id)
+                {
+                    return prce;
+                }
+            }
+
+            return null;
+        }
+    }
+}
