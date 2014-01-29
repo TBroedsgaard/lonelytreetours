de4aeae9f67e9e777c5646f70bf1336a7962cb23
Biacki <cille@valeria.dk>
2014-01-29 10:07:44 +0100

    DataAccessFacade

--- LonelyTreeTimeLimits/DataAccess/DataAccessFacade.cs
+++ LonelyTreeTimeLimits/DataAccess/DataAccessFacade.cs
@@ -13,11 +13,23 @@ namespace DataAccess
     {
         CustomerController customerController;
         SaleController saleController;
+        PaymentRuleCatalogController paymentRuleCatalogController;
+        PaymentContractController paymentContractController;
+        PaymentRuleController paymentRuleController;
+        BookingController bookingController;
+        PaymentController paymentController;
+        SupplierController supplierController;
 
         public DataAccessFacade()
         { 
             customerController = new CustomerController();
             saleController = new SaleController();
+            paymentRuleCatalogController = new PaymentRuleCatalogController();
+            paymentContractController = new PaymentContractController();
+            paymentRuleController = new PaymentRuleController();
+            bookingController = new BookingController();
+            paymentController = new PaymentController();
+            supplierController = new SupplierController(); 
         }
 
         public ICustomer CreateCustomer(ICustomer iCustomer)
@@ -75,68 +87,159 @@ namespace DataAccess
 
         public IPaymentRuleCatalog CreatePaymentRuleCatalog(IPaymentRuleCatalog iPaymentRuleCatalog)
         {
-            iPaymentRuleCatalog.Deleted = false;
+            iPaymentRuleCatalog = paymentRuleCatalogController.Create(iPaymentRuleCatalog);
+
+            return iPaymentRuleCatalog;
+        }
+
+        public IPaymentRuleCatalog UpdatePaymentRuleCatalog(IPaymentRuleCatalog iPaymentRuleCatalog)
+        {
+            iPaymentRuleCatalog = paymentRuleCatalogController.Update(iPaymentRuleCatalog);
+
+            return iPaymentRuleCatalog;
+        }
+
+        public IPaymentRuleCatalog DeletePaymentRuleCatalog(IPaymentRuleCatalog iPaymentRuleCatalog)
+        {
+            iPaymentRuleCatalog = paymentRuleCatalogController.Delete(iPaymentRuleCatalog);
+
             return iPaymentRuleCatalog;
         }
 
         public List<IPaymentRuleCatalog> GetPaymentRuleCatalogs()
         {
-            return new List<IPaymentRuleCatalog>();
+            return paymentRuleCatalogController.GetAll();
         }
 
         public IPaymentContract CreatePaymentContract(IPaymentContract iPaymentContract)
         {
-            // TODO: REMEMBER TO OVERWRITE ID, DELETED AND LAST UPDATED WHEN BUILDING FROM INTERFACE
-            // No, this is not an issue. I thought this was the ModelFacade. Just handle it with a controller as usual and move along :)
-            iPaymentContract.Deleted = false;
+            iPaymentContract = paymentContractController.Create(iPaymentContract);
+
             return iPaymentContract;
         }
 
-        public bool UpdatePaymentContract(IPaymentContract iPaymentContract)
+        public IPaymentContract UpdatePaymentContract(IPaymentContract iPaymentContract)
         {
-            return true;
+            iPaymentContract = paymentContractController.Update(iPaymentContract);
+
+            return iPaymentContract;
         }
 
-        public bool UpdatePaymentRuleCatalog(IPaymentRuleCatalog iPaymentRuleCatalog)
+        public IPaymentContract DeletePaymentContract(IPaymentContract iPaymentContract)
         {
-            return true;
-        }
+            iPaymentContract = paymentContractController.Delete(iPaymentContract);
 
-        public List<IPaymentRule> GetPaymentRules()
-        {
-            return new List<IPaymentRule>();
+            return iPaymentContract;
         }
 
         public List<IPaymentContract> GetPaymentContracts()
         {
-            return new List<IPaymentContract>();
-        }
-
-        public List<IBooking> GetBookings()
-        {
-            return new List<IBooking>();
-        }
-
-        public IBooking CreateBooking(IBooking iBooking)
-        {
-            iBooking.Deleted = false;
-            return iBooking;
-        }
-
-        public bool UpdateBooking(IBooking iBooking)
-        {
-            return true;
+            return paymentContractController.GetAll();
         }
 
         public IPaymentRule CreatePaymentRule(IPaymentRule iPaymentRule)
         {
-            iPaymentRule.Deleted = false;
+            iPaymentRule = paymentRuleController.Create(iPaymentRule);
+
             return iPaymentRule;
         }
 
-        public bool UpdatePaymentRule(IPaymentRule iPaymentRule)
+        public IPaymentRule UpdatePaymentRule(IPaymentRule iPaymentRule)
         {
-            return true;
+            iPaymentRule = paymentRuleController.Update(iPaymentRule);
+
+            return iPaymentRule;
         }
+
+        public IPaymentRule DeletePaymentRule(IPaymentRule iPaymentRule)
+        {
+            iPaymentRule = paymentRuleController.Delete(iPaymentRule);
+
+            return iPaymentRule;
+        }
+
+        public List<IPaymentRule> GetPaymentRules()
+        {
+            return paymentRuleController.GetAll();
+        }
+
+        public IBooking CreateBooking(IBooking iBooking)
+        {
+            iBooking = bookingController.Create(iBooking);
+
+            return iBooking;
+        }
+
+        public IBooking UpdateBooking(IBooking iBooking)
+        {
+            iBooking = bookingController.Update(iBooking);
+
+            return iBooking;
+        }
+
+        public IBooking DeleteBooking(IBooking iBooking)
+        {
+            iBooking = bookingController.Delete(iBooking);
+
+            return iBooking;
+        }
+
+        public List<IBooking> GetBookings()
+        {
+            return bookingController.GetAll();
+        }
+
+        public IPayment CreatePayment(IPayment iPayment)
+        {
+            iPayment = paymentController.Create(iPayment);
+
+            return iPayment;
+        }
+
+        public IPayment UpdatePayment(IPayment iPayment)
+        {
+            iPayment = paymentController.Update(iPayment);
+
+            return iPayment;
+        }
+
+        public IPayment DeletePayment(IPayment iPayment)
+        {
+            iPayment = paymentController.Delete(iPayment);
+
+            return iPayment;
+        }
+
+        public List<IPayment> GetPayments()
+        {
+            return paymentController.GetAll();
+        }
+
+        public ISupplier CreateSupplier(ISupplier iSupplier)
+        {
+            iSupplier = supplierController.Create(iSupplier);
+
+            return iSupplier;
+        }
+
+        public ISupplier UpdateSupplier(ISupplier iSupplier)
+        {
+            iSupplier = supplierController.Update(iSupplier);
+
+            return iSupplier;
+        }
+
+        public ISupplier DeleteSupplier(ISupplier iSupplier)
+        {
+            iSupplier = supplierController.Delete(iSupplier);
+
+            return iSupplier;
+        }
+
+        public List<ISupplier> GetSuppliers()
+        {
+            return supplierController.GetAll();
+        }
+
     }
 }
