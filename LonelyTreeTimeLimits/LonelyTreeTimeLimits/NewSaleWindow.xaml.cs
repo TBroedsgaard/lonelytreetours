767d381f07d5840c52b105ed1500498e2c169061
TBroedsgaard <troels@broedsgaard.com>
2014-01-30 10:38:12 +0100

    add booking to sale, edit sale

--- LonelyTreeTimeLimits/LonelyTreeTimeLimits/NewSaleWindow.xaml.cs
+++ LonelyTreeTimeLimits/LonelyTreeTimeLimits/NewSaleWindow.xaml.cs
@@ -20,39 +20,94 @@ namespace LonelyTreeTimeLimits
     /// </summary>
     public partial class NewSaleWindow : Window
     {
-        ModelFacade mf;
-        public NewSaleWindow(ModelFacade modfac)
+        ModelFacade modelFacade;
+        ISale sale;
+        public NewSaleWindow(ModelFacade modelFacade)
         {
             InitializeComponent();
-            mf = new ModelFacade();
-            mf = modfac; 
-            DateLabel.Content = DateTime.Now;
+            this.modelFacade = modelFacade;
 
+            sale = this.modelFacade.CreateSale();
+            sale.CreatedDate = DateTime.Now;
 
-            CustomerCombo.ItemsSource = mf.GetCustomerNames();
-            TypeCombo.ItemsSource = mf.GetCustomerTypes();
+            sale = this.modelFacade.UpdateSale(sale);
 
-            
+            DateLabel.Content = sale.CreatedDate;
 
-            
+            CustomerCombo.ItemsSource = modelFacade.GetCustomers();
+            customerTypeCombo.ItemsSource = getCustomerTypes();
 
+            StatusCombo.ItemsSource = getSaleStatusTypes();
+
+            BookingsListbox.ItemsSource = modelFacade.GetBookings(sale);
+        }
+
+        public NewSaleWindow(ModelFacade modelFacade, ISale sale)
+        {
+            InitializeComponent();
+            this.modelFacade = modelFacade;
+            this.sale = sale;
+
+            DateLabel.Content = this.sale.CreatedDate.ToShortDateString();
+
+            if (this.sale.SpecialRequests != null)
+                specialRequestTextbox.Text = this.sale.SpecialRequests;
+
+            CustomerCombo.ItemsSource = modelFacade.GetCustomers();
+            customerTypeCombo.ItemsSource = getCustomerTypes();
+
+            if (this.sale.Customer != null)
+                CustomerCombo.SelectedItem = this.sale.Customer; // Virker ikke
+
+            StatusCombo.ItemsSource = getSaleStatusTypes();
+
+            BookingsListbox.ItemsSource = modelFacade.GetBookings(sale);
+        }
+
+        private List<string> getCustomerTypes()
+        {
+            return Enum.GetNames(typeof(CustomerType)).ToList();
+        }
+
+        private List<string> getSaleStatusTypes()
+        { 
+            return Enum.GetNames(typeof(SaleStatus)).ToList();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            NewCustomerDialog customerDialog = new NewCustomerDialog(mf);
+            NewCustomerDialog customerDialog = new NewCustomerDialog(modelFacade);
             customerDialog.Show();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            BookingsWindow bk = new BookingsWindow();
+            BookingsWindow bk = new BookingsWindow(modelFacade, sale);
             bk.Show();
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
            
+        }
+
+        private void saveSaleButton_Click(object sender, RoutedEventArgs e)
+        {
+            sale.Customer = (ICustomer)CustomerCombo.SelectedItem;
+            sale.SaleStatus = (SaleStatus)Enum.Parse(typeof(SaleStatus), StatusCombo.SelectedItem.ToString()); //StatusCombo.SelectedItem
+            sale.SpecialRequests = specialRequestTextbox.Text;
+
+            modelFacade.UpdateSale(sale);
+
+            Close();
+        }
+
+        private void editBookingButton_Click(object sender, RoutedEventArgs e)
+        {
+            IBooking booking = (IBooking)BookingsListbox.SelectedItem;
+            BookingsWindow bk = new BookingsWindow(modelFacade, booking);
+
+            bk.Show();
         }
     }
 }
