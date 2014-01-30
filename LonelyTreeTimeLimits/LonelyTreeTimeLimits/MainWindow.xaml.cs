92d040acc844924277373b5a2d681a5c087e5bd6
TBroedsgaard <troels@broedsgaard.com>
2014-01-30 10:42:49 +0100

    small fixes to MainWindow

--- LonelyTreeTimeLimits/LonelyTreeTimeLimits/MainWindow.xaml.cs
+++ LonelyTreeTimeLimits/LonelyTreeTimeLimits/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Model;
+using Interfaces;
 
 
 
@@ -23,35 +24,55 @@ namespace LonelyTreeTimeLimits
     /// </summary>
     public partial class MainWindow : Window
     {   
-        ModelFacade Mf;
+        ModelFacade modelFacade;
       
         public MainWindow()
         {
-              InitializeComponent();
-              Mf = new ModelFacade();
-              List<string> custList = new List<string>();
-              
-               
-            SalesListBox.ItemsSource =  Mf.GetCustomerNames();
-             
-              
-              
+            InitializeComponent();
+            modelFacade = new ModelFacade();
+            List<ISale> sales = modelFacade.GetSales();
+            salesListView.ItemsSource = getActiveSales();
+        }
+
+        private List<ISale> getActiveSales()
+        {
+            List<ISale> activeSales = new List<ISale>();
+
+            List<ISale> allSales = modelFacade.GetSales();
+
+            foreach (ISale sale in allSales)
+            {
+                if (sale.Deleted == false)
+                {
+                    activeSales.Add(sale);
+                }
+            }
+
+            return activeSales;
         }
 
         private void salesDeleteButton_Click(object sender, RoutedEventArgs e)
         {
-            
-
+            ISale sale = salesListView.SelectedItem as ISale;
+            modelFacade.DeleteSale(sale);
         }
 
         private void saleNewButton_Click(object sender, RoutedEventArgs e)
         {
-         
-             NewSaleWindow NewSaleWindow = new NewSaleWindow(Mf);
+             NewSaleWindow NewSaleWindow = new NewSaleWindow(modelFacade);
              NewSaleWindow.Show();
-             Mf.CreateSale();
-             SalesListBox.Items.Refresh();
+             NewSaleWindow.Closed += NewSaleWindow_Closed;
+        }
 
+        void NewSaleWindow_Closed(object sender, EventArgs e)
+        {
+             salesListView.ItemsSource = getActiveSales();
+        }
+
+        private void salesEditButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewSaleWindow editSaleWindow = new NewSaleWindow(modelFacade, (ISale)salesListView.SelectedItem);
+            editSaleWindow.Show();
 
         }
     }
